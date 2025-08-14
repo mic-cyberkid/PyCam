139bb23ee6885a6f2d8203b10bc503fba7fd9ead
mic-cyberkid <93459729+mic-cyberkid@users.noreply.github.com>
2025-08-14 11:59:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import kivy
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from screens.camera_screen import CameraScreen
-# Import the gallery screen later
 from screens.gallery_screen import GalleryScreen
 
 class PyCamApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(CameraScreen(name='camera_screen'))
-        # sm.add_widget(GalleryScreen(name='gallery_screen'))
+        sm.add_widget(GalleryScreen(name='gallery_screen'))
         return sm
 
 if __name__ == '__main__':
